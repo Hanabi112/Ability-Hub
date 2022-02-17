edce1b399790d33eecddf528e52e302a3d536f28
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-17 23:08:13 +0700

    Update AnimePunchingSimulator.lua

--- AnimePunchingSimulator.lua
+++ AnimePunchingSimulator.lua
@@ -5,6 +5,7 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Patsk
 
 local GUI = library:new("Ability Hub","[ RightControl ]")
 local Main = GUI:Tap("Mian")
+local Misc = GUI:Tap("Misc")
 
 local PlaceId = game.PlaceId
 Magnet = true
@@ -66,4 +67,83 @@ AutoEgg = VValue
             game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer(unpack(args))
         end
     end
+end)
+
+Main:Button("Egg Shop",function()
+      local args = {
+                [1] = "EGG",
+                [2] = workspace.__SETTINGS.__INTERACT.EGG,
+                [3] = "3",
+                [4] = {}
+            }
+
+            game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer(unpack(args))
+end)
+
+Misc:Button("Server Hop",function()
+        local PlaceID = game.PlaceId
+    local AllIDs = {}
+    local foundAnything = ""
+    local actualHour = os.date("!*t").hour
+    local Deleted = false
+    function TPReturner()
+        local Site;
+        if foundAnything == "" then
+            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+        else
+            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+        end
+        local ID = ""
+        if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+            foundAnything = Site.nextPageCursor
+        end
+        local num = 0;
+        for i,v in pairs(Site.data) do
+            local Possible = true
+            ID = tostring(v.id)
+            if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                for _,Existing in pairs(AllIDs) do
+                    if num ~= 0 then
+                        if ID == tostring(Existing) then
+                            Possible = false
+                        end
+                    else
+                        if tonumber(actualHour) ~= tonumber(Existing) then
+                            local delFile = pcall(function()
+                                -- delfile("NotSameServers.json")
+                                AllIDs = {}
+                                table.insert(AllIDs, actualHour)
+                            end)
+                        end
+                    end
+                    num = num + 1
+                end
+                if Possible == true then
+                    table.insert(AllIDs, ID)
+                    wait()
+                    pcall(function()
+                        -- writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                        wait()
+                        game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                    end)
+                    wait(.1)
+                end
+            end
+        end
+    end
+    function Teleport() 
+        while wait() do
+            pcall(function()
+                TPReturner()
+                if foundAnything ~= "" then
+                    TPReturner()
+                end
+            end)
+        end
+    end
+    Teleport()
+end)
+
+Misc:Button("Rejoin",function()
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").localPlayer)
 end)
